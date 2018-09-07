e006378447cc7c46a830e8705471ae982fbb2b3c
Alex Chum <chumalexander96@gmail.com>
2018-09-07 18:38:53 -0400

    Add Save and Load mechanism

---
+++ Game/Assets/Script/GameControl/GameController.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/*
+ * GameController manages input affecting the game state.
+ * 
+ * Only one GameController should exist at a time
+ */
+public class GameController : MonoBehaviour {
+
+    public static GameController gameController;
+
+    private string _persistentFilesPath;
+
+    private float startTime;
+    private float lastLoadTime;
+    private float gameplayTime;
+    private SaveFileData saveFileData;
+
+    void Awake()
+    {
+        // Singleton Pattern
+        if (gameController == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameController = this;
+        }
+        else if (gameController != this)
+        {
+            Destroy(gameObject);
+        }
+        startTime = Time.time;
+        lastLoadTime = 0.0f;
+        gameplayTime = 0.0f;
+        _persistentFilesPath = Application.persistentDataPath;
+    }
+
+    void Update () {
+
+        // Escape Key Press
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            PauseMenuController.pauseMenuController.TriggerPauseMenu();
+        }
+
+	}
+
+    public void QuitGame() {
+        // IF IN UNITY EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        // ELSE 
+        Application.Quit();
+    }
+
+    public void SaveGame(string saveSlotNumber) {
+
+        // File name and path setup
+        string fileName = "/saveFile" + saveSlotNumber + ".dat";
+        string filePath = _persistentFilesPath + fileName;
+
+        // If file already exits ...
+        if (File.Exists(filePath)) {
+            // ... Prompt User ...
+            // TODO: Modal Window - Return if canceled
+        }
+        FileStream file = File.Create(filePath);
+
+
+        // Saved Data Initialization
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        SaveFileData data = new SaveFileData
+        {
+            // TODO: Add Data to be saved
+            sceneName = SceneManager.GetActiveScene().name,
+            characterPositionX = player.transform.position.x,
+            characterPositionY = player.transform.position.y,
+            characterPositionZ = player.transform.position.z,
+            totalPlayTime = Time.time - startTime - lastLoadTime + gameplayTime
+        };
+
+        // Write File 
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+
+        Debug.Log("Game Saved Successfully!");
+        Debug.Log("File Stored in path: " + filePath);
+    }
+
+    public void LoadGame(string saveSlotNumber) {
+        // File name and path setup
+        string fileName = "/saveFile" + saveSlotNumber + ".dat";
+        string filePath = _persistentFilesPath + fileName;
+
+        if (File.Exists(filePath)) {
+            // Read File
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(filePath, FileMode.Open);
+            saveFileData = (SaveFileData) bf.Deserialize(file);
+            file.Close();
+
+            // TODO: Prompt user to load file
+
+            // Load Data into the game
+            SceneManager.LoadScene(saveFileData.sceneName);
+            // TODO: Add Data to be loaded
+            StartCoroutine(LoadDataOnceSceneLoaded(saveFileData));
+
+            gameplayTime = saveFileData.totalPlayTime;
+            lastLoadTime = Time.time;
+        }
+
+    }
+
+    /*
+     * Loads data once scene is loaded
+     * 
+     * Note: This function is not absolute especially if scene 
+     * takes a long time to load.
+     */
+    IEnumerator LoadDataOnceSceneLoaded(SaveFileData data) {
+
+        // Wait for scene to load
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        // TODO: Add Data to be loaded ONCE SCENE IS DONE LOADING
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerPos = new Vector3(data.characterPositionX,
+                                                data.characterPositionY,
+                                                data.characterPositionZ);
+        player.transform.position = playerPos;
+
+        Debug.Log("Data Loaded");
+        // Stops when the player is at the right position (reference point)
+        if (player.transform.position == playerPos) {
+            yield break;
+        }
+    }
+}
+
+
+[Serializable]
+/*
+ * Struct containing all the data needed to load a gameplay
+ */
+struct SaveFileData {
+    // TODO: Add Data to be saved
+    public string sceneName;
+    public float characterPositionX;
+    public float characterPositionY;
+    public float characterPositionZ;
+    public float totalPlayTime;
+}
---
+++ Game/Assets/Script/UI/PauseMenuController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*
+ * Pause Menu control 
+ * 
+ * Only one PauseMenu script should exist at a time
+ * This script should be attached to the Canvas of the Pause Menu
+ */
+public class PauseMenuController : MonoBehaviour
+{
+
+    public static PauseMenuController pauseMenuController;
+
+    void Awake()
+    {
+        // Singleton Pattern
+        if (pauseMenuController == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            pauseMenuController = this;
+        }
+        else if (pauseMenuController != this)
+        {
+            Destroy(gameObject);
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    /*
+    * Calls in Game Menu
+    * 
+    * Updates the current game status accordingly.
+    */
+    public void TriggerPauseMenu()
+    {
+        if (gameObject.activeSelf)
+        {
+            string activeMenu = GetActiveMenu();
+            if (activeMenu == "BaseMenu")
+            {
+                // Close UI
+                gameObject.SetActive(false);
+                // Enable musics
+                // Enable controls (using timescale, up for change)
+                Time.timeScale = 1.0f;
+            }
+            else
+            {
+                SetActiveMenu("BaseMenu");
+            }
+            // Deselect any buttons
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+        else
+        {
+            // Disable controls (using timescale, up for change)
+            Time.timeScale = 0.0f;
+            // Disable musics
+            // Open UI
+            SetActiveMenu("BaseMenu");
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void SetActiveMenu(string menuTag)
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.tag == menuTag)
+                child.gameObject.SetActive(true);
+            else
+                child.gameObject.SetActive(false);
+        }
+    }
+
+    /*
+     * Return the current active menu
+     * 
+     * Note: There should always be an active menu
+     */
+    private string GetActiveMenu() {
+        foreach (Transform child in transform) {
+            if (child.gameObject.activeSelf) return child.gameObject.tag;
+        }
+        return null;
+    }
+}
