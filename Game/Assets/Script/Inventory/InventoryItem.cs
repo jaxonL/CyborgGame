e20f072fac46c722e6780be18f3d96eeb6a85871
Alex Chum <chumalexander96@gmail.com>
2018-09-20 11:32:36 -0400

    Create CollectibleItem Script and Inventory System

---
+++ Game/Assets/Script/Inventory/InventoryItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public string itemName;
+    public Sprite sprite;
+    public string description;
+
+    private Text displayedDescription;
+    // Use this for initialization
+    private void FindDisplayedText()
+    {
+        if (displayedDescription == null) {
+            displayedDescription = GameObject.FindGameObjectWithTag("ItemDescription").GetComponent<Text>();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        DisplayText();
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        StopDisplayText();
+    }
+
+    private void DisplayText()
+    {
+        FindDisplayedText();
+        displayedDescription.text = description;
+    }
+    private void StopDisplayText()
+    {
+        FindDisplayedText();
+        displayedDescription.text = "";
+    }
+}
