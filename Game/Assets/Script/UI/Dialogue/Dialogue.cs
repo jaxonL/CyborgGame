b33dccb0d2bee16015c01a3bdc905ec837245ec0
jaxonl <lin.jiaxuan@hotmail.com>
2018-11-25 00:55:59 -0500

    feat(Dialogue): take in a text file and parse the text by line between start/end tags

---
+++ Game/Assets/Script/UI/Dialogue/Dialogue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+    public TextAsset rawDialogue;
+
+    public string[] pureText;
+
+    private static string START_TAG = "[start]";
+    private static string END_TAG = "[end]";
+
+    public void StripStartEndTags()
+    {
+        Debug.Log(rawDialogue.text);
+        int startIndex = rawDialogue.text.IndexOf(START_TAG) + START_TAG.Length;
+        if(startIndex < 0)
+        {
+            throw new System.Exception("Error: Expected " + rawDialogue.name + " to contain a '[start]' tag.");
+        }
+        // the first index we DON'T need
+        int endIndex = rawDialogue.text.IndexOf(END_TAG);
+        if (endIndex < 0)
+        {
+            throw new System.Exception("Error: Expected " + rawDialogue.name + " to contain an '[end]' tag.");
+        }
+
+        string dialogueMeat = rawDialogue.text.Substring(startIndex, endIndex - startIndex);
+
+        Debug.Log("meat: " + dialogueMeat);
+
+        pureText = dialogueMeat.Split(
+            new[] { "\r\n", "\r", "\n" },
+            System.StringSplitOptions.None)
+            .Where((line) => {
+                return !string.IsNullOrEmpty(line.Trim());
+            })
+            .ToArray();
+    }
+}
---
+++ Game/Assets/Script/UI/Dialogue/DialogueManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+    private static string CHAR_NAME_DELIMITER = "::";
+    private static string VARIABLE_OPENER = "{{";
+    private static string VARIABLE_CLOSER = "}}";
+
+    private Queue<string> linesQueue;
+
+	void Start () {
+        linesQueue = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation");
+        linesQueue.Clear();
+
+        foreach(string line in dialogue.pureText)
+        {
+            // TODO: process line
+            linesQueue.Enqueue(line);
+        }
+
+        DisplayNextLine();
+    }
+
+    public void DisplayNextLine() {
+        if(linesQueue.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string line = linesQueue.Dequeue();
+        Debug.Log(line.ToString());
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
